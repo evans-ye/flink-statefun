fee3e5f4556bdfa1764b5e6bb277204db3471451
Evans Ye <evansye@apache.org>
2021-06-14 19:56:44 +0800

    Turn smoke e2e from embedded to remote function

---
+++ statefun-e2e-tests/statefun-smoke-e2e-java/statefun-smoke-e2e-fn/src/main/java/org/apache/flink/statefun/e2e/smoke/CommandInterpreter.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flink.statefun.e2e.smoke;
+
+import org.apache.flink.statefun.e2e.smoke.generated.Command;
+import org.apache.flink.statefun.e2e.smoke.generated.Commands;
+import org.apache.flink.statefun.e2e.smoke.generated.SourceCommand;
+import org.apache.flink.statefun.e2e.smoke.generated.VerificationResult;
+import org.apache.flink.statefun.sdk.FunctionType;
+import org.apache.flink.statefun.sdk.java.*;
+import org.apache.flink.statefun.sdk.java.message.EgressMessageBuilder;
+import org.apache.flink.statefun.sdk.java.message.Message;
+import org.apache.flink.statefun.sdk.java.message.MessageBuilder;
+import org.apache.flink.statefun.sdk.java.types.SimpleType;
+import org.apache.flink.statefun.sdk.java.types.Type;
+import org.apache.flink.statefun.sdk.reqreply.generated.TypedValue;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import static org.apache.flink.statefun.flink.common.types.TypedValueUtil.packProtobufMessage;
+
+public final class CommandInterpreter {
+  private final Ids ids;
+  private static final Duration sendAfterDelay = Duration.ofMillis(1);
+
+  static final Type<Command> COMMAND_TYPE = SimpleType.simpleImmutableTypeFrom(
+      TypeName.typeNameOf("statefun.smoke.e2e.types", "command"),
+      Command::toByteArray,
+      bytes -> Command.parser().parseFrom(bytes));
+
+  static final Type<Commands> COMMANDS_TYPE = SimpleType.simpleImmutableTypeFrom(
+      TypeName.typeNameOf("statefun.smoke.e2e.types", "commands"),
+      Commands::toByteArray,
+      bytes -> Commands.parser().parseFrom(bytes));
+
+  static final Type<SourceCommand> SOURCE_COMMAND_TYPE = SimpleType.simpleImmutableTypeFrom(
+      TypeName.typeNameOf("statefun.smoke.e2e.types", "source-command"),
+      SourceCommand::toByteArray,
+      bytes -> SourceCommand.parser().parseFrom(bytes));
+
+  static final Type<VerificationResult> VERIFICATION_RESULT_TYPE = SimpleType.simpleImmutableTypeFrom(
+      TypeName.typeNameOf("statefun.smoke.e2e.types", "verification-result"),
+      VerificationResult::toByteArray,
+      bytes -> VerificationResult.parser().parseFrom(bytes));
+
+  static final TypeName FLINK_SINK_EGRESS_TYPENAME =
+      TypeName.typeNameOf("statefun.smoke.e2e.types", "flink-sink-egress");
+
+  public CommandInterpreter(Ids ids) {
+    this.ids = Objects.requireNonNull(ids);
+  }
+
+  public void interpret(ValueSpec<Long> state, Context context, Message message) {
+    if (message.is(SOURCE_COMMAND_TYPE)) {
+      interpret(state, context, message.as(SOURCE_COMMAND_TYPE).getCommands());
+    } else if (message.is(COMMANDS_TYPE)) {
+      interpret(state, context, message.as(COMMANDS_TYPE));
+    } else {
+      throw new IllegalArgumentException("Unrecognized message type!");
+    }
+  }
+
+  private void interpret(ValueSpec<Long> state, Context context, Commands cmds) {
+    for (Command cmd : cmds.getCommandList()) {
+      if (cmd.hasIncrement()) {
+        modifyState(state, context, cmd.getIncrement());
+      } else if (cmd.hasSend()) {
+        send(state, context, cmd.getSend());
+      } else if (cmd.hasSendAfter()) {
+        sendAfter(state, context, cmd.getSendAfter());
+      } else if (cmd.hasSendEgress()) {
+        sendEgress(state, context, cmd.getSendEgress());
+      } else if (cmd.hasVerify()) {
+        verify(state, context, cmd.getVerify());
+      }
+    }
+  }
+
+  private void verify(
+      ValueSpec<Long> state,
+      @SuppressWarnings("unused") Context context,
+      Command.Verify verify) {
+    AddressScopedStorage storage = context.storage();
+    Address targetAddress = new Address(Fn.TYPENAME, context.self().id());
+    long actual = storage.get(state).orElse(0L);
+    long expected = verify.getExpected();
+    VerificationResult verificationResult =
+        VerificationResult.newBuilder()
+            .setId(Integer.parseInt(context.self().id()))
+            .setActual(actual)
+            .setExpected(expected)
+            .build();
+    Message message =
+        MessageBuilder.forAddress(targetAddress)
+            .withCustomType(VERIFICATION_RESULT_TYPE, verificationResult)
+            .build();
+    context.send(message);
+  }
+
+  private void sendEgress(
+      @SuppressWarnings("unused") ValueSpec<Long> state,
+      Context context,
+      @SuppressWarnings("unused") Command.SendEgress sendEgress) {
+    // TODO: Not sure whether this is the desired logic.
+    Message message =
+        EgressMessageBuilder.forEgress(FLINK_SINK_EGRESS_TYPENAME)
+            .withCustomType(COMMAND_TYPE, Command.getDefaultInstance());
+    context.send(message);
+  }
+
+  private void sendAfter(
+      @SuppressWarnings("unused") ValueSpec<Long> state,
+      Context context,
+      Command.SendAfter send) {
+    String id = ids.idOf(send.getTarget());
+    Address targetAddress = new Address(Fn.TYPENAME, id);
+    Message message =
+        MessageBuilder.forAddress(targetAddress)
+            .withCustomType(COMMANDS_TYPE, send.getCommands())
+            .build();
+    context.sendAfter(sendAfterDelay, message);
+  }
+
+  private void send(
+      @SuppressWarnings("unused") ValueSpec<Long> state, Context context, Command.Send send) {
+    String id = ids.idOf(send.getTarget());
+    Address targetAddress = new Address(Fn.TYPENAME, id);
+    Message message =
+        MessageBuilder.forAddress(targetAddress)
+            .withCustomType(COMMANDS_TYPE, send.getCommands())
+            .build();
+    context.send(message);
+  }
+
+  private void modifyState(
+      ValueSpec<Long> state,
+      @SuppressWarnings("unused") Context context,
+      @SuppressWarnings("unused") Command.IncrementState incrementState) {
+    // updateAndGet -> get+set concurrent modification?
+    AddressScopedStorage storage = context.storage();
+    long n = storage.get(state).orElse(0L);
+    storage.set(state, n + 1);
+  }
+}
